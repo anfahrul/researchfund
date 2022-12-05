d7ec1c7fef44449a52d998804f2b9ae19abf9fd5
anfahrul <an.fahrulan@gmail.com>
2022-12-05 17:15:33 +0800

    initial commit

---
+++ src/main/kotlin/com/anfahrul/researchfund/ResearchfundApplication.kt
@@ -0,0 +1,11 @@
+package com.anfahrul.researchfund
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ResearchfundApplication
+
+fun main(args: Array<String>) {
+	runApplication<ResearchfundApplication>(*args)
+}
---
+++ src/test/kotlin/com/anfahrul/researchfund/ResearchfundApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.anfahrul.researchfund
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ResearchfundApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
